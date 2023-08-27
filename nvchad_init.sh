a269e2df62eaa49c235ece0ecf0898809283c488
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2023-08-27 12:49:34 +0200

    Update nvchad_init.sh

--- nvchad_init.sh
+++ nvchad_init.sh
@@ -5,15 +5,12 @@ echo "install my nvchad for neovim"
 if command -v apt &> /dev/null; then
     echo "Using APT package manager:"
     sudo apt update && sudo apt install neovim
-# Check if YUM is available
 elif command -v yum &> /dev/null; then
     echo "Using YUM package manager:"
     sudo yum install neovim
-# Check if DNF is available
 elif command -v dnf &> /dev/null; then
     echo "Using DNF package manager:"
     sudo dnf install neovim
-# Check if Pacman is available
 elif command -v pacman &> /dev/null; then
     echo "Using Pacman package manager:"
     sudo pacman -S neovim
